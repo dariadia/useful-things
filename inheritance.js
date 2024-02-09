f403096556c74285d08ad2e8419395f9f1a67709
Daria V. Diachkova <daria.diachkova@mail.ru>
2024-02-09 14:46:37 +0100

    Update inheritance.js

--- inheritance.js
+++ inheritance.js
@@ -9,11 +9,21 @@ me.talk()
 // To update:
 Person.prototype.talk = () => console.log('New! I am talking')
 
+class SuperHuman extends Person {
+  fly = () => console.log('I am flying')
+}
+const me = new SuperHuman()
+me.talk()
+me.fly()
+
+
+
 // Inheritance using a Constructor Function
 function Person() {}
 Person.prototype.talk = () => console.log('Talking')
 const me = new Person()
 me.talk()
+
 
 
 // Inheritance using pure objects with Object.create
@@ -23,3 +33,11 @@ const person = {
 const me = Object.create(person)
 me.talk()
 
+
+
+// Inheritance using pure objects with Object.setPrototypeOf
+const person = {}
+person.__proto__.talk = () => console.log('Talking')
+const me = {}
+Object.setPrototypeOf(me, person)
+me.talk()
