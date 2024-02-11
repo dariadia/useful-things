824ded5e4c399d119f19e7a3ad598b9b65241ed2
Daria V. Diachkova <daria.diachkova@mail.ru>
2024-02-11 17:21:38 +0100

    Update this.js

--- this.js
+++ this.js
@@ -15,11 +15,13 @@ mocking() // === window.mocking()
 
 
 
-function talk() {
-  return 'I am ${this.name}
+function talk(arg) {
+  return `I am ${this.name}${arg}`
 }
 const me = { name: 'Jane' ｝
 const meTalk = talk.bind(me) // bind "this" here to (me)
 meTalk() // "I am Jane"
 
+talk.call(me, ...arrayArgs) // call this function with var = this, so === "I am Jane"
+talk.apply(me, [arg1, arg2]) // same as above, different syntax
 
