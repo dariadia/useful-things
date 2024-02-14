cd52c46b3547dd8d6d3f32a77f74c785b7d435b0
Daria V. Diachkova <daria.diachkova@mail.ru>
2024-02-14 23:12:34 +0100

    Create dom-api.js

---
+++ dom-api.js
@@ -0,0 +1,14 @@
+/* 
+The Document Object Model (DOM) is the data representation of the objects 
+that comprise the structure and content of a document on the web. 
+Basically: how your data gets on the webpage.
+
+GOTCHA:
+The presence of whitespace in the DOM can cause layout problems 
+and make manipulation of the content tree difficult in unexpected ways, 
+depending on where it is located. This article explores 
+when difficulties can occur, and looks at what 
+can be done to mitigate resulting problems.
+
+
+*/
