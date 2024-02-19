38355d2dff514437822d56da7c31c23cc5b92df7
Daria V. Diachkova <daria.diachkova@mail.ru>
2024-02-19 11:24:51 +0100

    Create web-api.js

---
+++ web-api.js
@@ -0,0 +1,29 @@
+// JavaScript Validation API
+checkValidity()	// => true if an input element contains valid data.
+setCustomValidity("Some error text goes here")	// Sets the validationMessage property of an input element.
+
+validity //	Contains boolean properties related to the validity of an input element.
+validationMessage	// Contains the message a browser will display when the validity is false.
+willValidate // Indicates if an input element will be validated.
+customError	// Set to true, if a custom validity message is set.
+patternMismatch	// Set to true, if an element's value does not match its pattern attribute.
+rangeOverflow	// Set to true, if an element's value is greater than its max attribute.
+rangeUnderflow	// Set to true, if an element's value is less than its min attribute.
+stepMismatch	// Set to true, if an element's value is invalid per its step attribute.
+tooLong	// Set to true, if an element's value exceeds its maxLength attribute.
+typeMismatch	// Set to true, if an element's value is invalid per its type attribute.
+valueMissing	// Set to true, if an element (with a required attribute) has no value.
+valid	// Set to true, if an element's value is valid.
+
+function validate(inputID) {
+  const input = document.getElementById(inputID)
+  const validityState = input.validity
+  if (validityState.valueMissing) {
+    input.setCustomValidity("You gotta fill this out, yo!")
+  } else if (validityState.rangeUnderflow) {
+    input.setCustomValidity("We need a higher number!")
+  } else if (validityState.rangeOverflow) {
+    input.setCustomValidity("Thats too high!")
+  } else input.setCustomValidity("")
+  input.reportValidity()
+}
